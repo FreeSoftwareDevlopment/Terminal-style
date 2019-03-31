b16321cb441e52088a44cd2ee50fb7df8509d57a
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2019-03-31 20:02:07 +0200

    Add files via upload

---
+++ stylelist/src/Gruntfile.js
@@ -0,0 +1,15 @@
+﻿module.exports = function(grunt) {
+
+  grunt.initConfig({
+    cssmin: {
+		compress:{
+            files: {'min.css':['css/*.css']},
+		},
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['cssmin']);
+
+};
